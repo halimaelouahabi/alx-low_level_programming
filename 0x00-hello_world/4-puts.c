a2a642fdca710c9298885ef331a5cbb06adac927
halimaelouahabi <halima.elouahabi11@gmail.com>
2023-07-13 14:29:35 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,19 +2,19 @@
 
 /**
 
-* main - Entry point
+ * main - Entry point
 
-*
+ *
 
-* Return: Always 0 (Success)
+ * Return: Always 0 (Success)
 
-*/
+ */
 
 int main(void)
 
 {
 
-        printf("with proper grammar, but the outcome is a piece of art,\n");
+        puts("\"Programming is like building a multilingual puzzle");
 
         return (0);
 }
