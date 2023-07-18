399b4eacde1fd637c06bba31bff8392761edc19d
halimaelouahabi <halima.elouahabi11@gmail.com>
2023-07-18 09:20:18 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- * main - Entry point
- * description: 'prints _putchar to stdout'
- * return: always 0
+ * main - print _putchar
+ * is written by mbah
+ * Return: 0
  */
 int main(void)
 {
